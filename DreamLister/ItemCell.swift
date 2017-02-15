53ca334ebd78c3affeb314554f64caac0cb494c2
TaimoorSaeed <taimoor_saeed@hotmail.com>
2017-02-15 13:59:12 +0500

    NSFetchResultController

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Nano Degree on 15/02/2017.
+//  Copyright © 2017 Swift Pakistan. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+}
