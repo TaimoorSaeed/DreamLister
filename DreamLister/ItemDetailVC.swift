61d9619d4295ebd4a71db02a50ef1dcfb64be6ae
TaimoorSaeed <taimoor_saeed@hotmail.com>
2017-02-16 16:09:51 +0500

    pickerview-added

---
+++ DreamLister/ItemDetailVC.swift
@@ -0,0 +1,87 @@
+//
+//  ItemDetailVC.swift
+//  DreamLister
+//
+//  Created by Nano Degree on 16/02/2017.
+//  Copyright © 2017 Swift Pakistan. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ItemDetailVC: UIViewController, UIPickerViewDelegate , UIPickerViewDataSource {
+
+    
+    @IBOutlet weak var storePicker : UIPickerView!
+    @IBOutlet weak var titleFeild : CustomTextField!
+    @IBOutlet weak var PriceFeild : CustomTextField!
+    @IBOutlet weak var detailsField : CustomTextField!
+    
+    
+    var stores = [Store]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let topItem = self.navigationController?.navigationBar.topItem {
+            
+            topItem.backBarButtonItem = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.plain, target: nil , action: nil)
+        }
+        
+        storePicker.delegate = self
+        storePicker.dataSource = self
+        
+        
+//        let store = Store(context: context)
+//        store.name = "Best Buy"
+//        let store2 = Store(context: context)
+//        store2.name = "Tesla Dealarship"
+//        let store3 = Store(context: context)
+//        store3.name = "Fry Electonics"
+//        let store4 = Store(context: context)
+//        store4.name = "Target"
+//        let store5 = Store(context: context)
+//        store5.name = "Amazon"
+//        let store6 = Store(context: context)
+//        store6.name = "K Mart"
+//        
+//        ad.saveContext()
+        getStores()
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        let store = stores[row]
+        return store.name
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return stores.count
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        //update when selected
+    }
+    
+    func getStores() {
+        
+        let fetchRequest: NSFetchRequest<Store> = Store.fetchRequest()
+        
+        do {
+            self.stores = try context.fetch(fetchRequest)
+            self.storePicker.reloadAllComponents()
+        } catch {
+            
+            // handle the error
+        }
+        
+    }
+    
+    
+}
